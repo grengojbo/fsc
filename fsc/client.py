c531960b077b2486be2c8469296fa5fe9ea3e409
Oleg Dolya <oleg.dolya@gmail.com>
2010-05-25 19:41:07 +0300

    Added RESTfull client new file:   fsc/client.py

---
+++ fsc/client.py
@@ -0,0 +1,49 @@
+# -*- mode: python; coding: utf-8; -*-
+import urllib2
+from urllib2 import urlparse
+import simplejson
+from restfull.client import Connection
+
+########################################################################
+class ClietAPI():
+    """"""
+
+    #----------------------------------------------------------------------
+    def __init__(self, base_url, options):
+        """ Base URL for the store should be pretty self-explanatory. E.g. something like
+            "http://api.linktel.com.ua/api"
+            Only needs to enter the username/password if this class is going to tinker
+             with things."""
+        if base_url.endswith('/'):
+            base_url = base_url[:-1]
+
+        self.base_url = base_url
+        # Split the given URL
+        if base_url:
+            self.conn = Connection(base_url, username=options.user, password=options.passwd)
+    
+    def _query_search(self, query, args={}):
+        """Low-level content box query - returns the message and response headers from the server.
+           You may be looking for Store.search instead of this."""
+           
+        passed_args = {}
+        passed_args.update(args)
+        return self.conn.request_get(query, args=passed_args, headers={'Accept':'text/json'})
+    
+    def search(self, query, args={}):
+        """Performs a search query and simply returns the body of the response if successful
+           - if there is an issue, such as a code 404 or 500, this method will return False. 
+           
+           Use the _query_search_service method to get hold of
+           the complete response in this case."""
+        response = self._query_search(query, args)
+        headers = response.get('headers')
+        status = headers.get('status', headers.get('Status'))
+        
+        if status in ['200', 200, '204', 204]:
+            body = simplejson.loads(response.get('body').encode('UTF-8'))
+            return body
+        else:
+            return False
+
+
